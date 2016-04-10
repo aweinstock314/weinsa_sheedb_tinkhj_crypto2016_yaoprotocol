3f3ff11b5a4f1dbfae3820eedf06c517156fb777
weableandbob <weableandbob@gmail.com>
2016-04-10 14:51:05 -0400

    Argument parsing and beginnings of evaluators

--- implementation/src/yaoprotocol.cpp
+++ implementation/src/yaoprotocol.cpp
@@ -1,6 +1,8 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <vector>
+#include <iostream>
+#include <string>
 
 #include "utils.h"
 
@@ -22,10 +24,70 @@ class TerriblyInsecureObliviousTransfer {
         read_aon(fd, (char*)y.data(), len);
         return bit ? y : x;
     }
+
+using namespace std;
+
+/******************************************************************************/
+/* Circuit class **************************************************************/
+/******************************************************************************/
+class Circuit{
+private:
+	int num_bits;
+public:
+	Circuit(int num_bits);
+};
+
+Circuit::Circuit(int num_bits){
+	this.num_bits = num_bits;
+}
+
+/******************************************************************************/
+/* SenderEvaluator class ******************************************************/
+/******************************************************************************/
+class SenderEvaluator{
+private:
+	Circuit ungarbled_circuit;
+	Circuit garbled_circuit;
+public:
+	SenderEvaluator(int num_bits);
 };
 
 int main(int argc, char** argv) {
     // silence -Wunused-parameter for skeleton
     (void)argc; (void)argv;
+
+    //Argument verification and parsing
+    if(argc != 4 && argc != 5){
+    	cout << "Usage is " << argv[0] << "sender <hostname> <port> <wealth>" << endl;
+    	cout << "OR" << endl;
+    	cout << "Usage is " << argv[0] << "receiver <port> <wealth>" << endl;
+    	return 1;
+    }
+    if(argc == 5 && strcmp("sender", argv[1]) != 0){
+    	cout << "Usage is " << argv[0] << "sender <hostname> <port> <wealth>" << endl;
+    	return 1;
+    }
+    if(argc == 4 && strcmp("receiver", argv[1]) != 0){
+    	cout << "Usage is " << argv[0] << "receiver <port> <wealth>" << endl;
+    	return 1;
+    }
+
+    string hostname;
+    unsigned short port;
+    uint64_t wealth;
+
+    if(argc == 4){
+    	port = (unsigned short) atoi(argv[2]);
+    	wealth = (uint64_t) atoi(argv[3]);
+    }
+    else{
+    	hostname = argv[2];
+    	port = (unsigned short) atoi(argv[3]);
+    	wealth = (uint64_t) atoi(argv[4]);
+    }
+
+
+
     printf("TODO: everything\n");
+    return 0;
 }
