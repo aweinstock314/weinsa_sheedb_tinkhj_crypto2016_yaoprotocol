2c6e56c19fd7b88d58292da6f85f74ac4875ea31
Brian Sheedy <weableandbob@gmail.com>
2016-04-24 14:39:05 -0400

    Finished OT send, but untested

--- implementation/src/oblivious_transfer.cpp
+++ implementation/src/oblivious_transfer.cpp
@@ -1,3 +1,5 @@
+//Oblivious transfer based off the RSA implementation on Wikipedia
+
 #include <iostream>
 //#include <crypto++.h>
 //#include <cryptopp.h>
@@ -8,11 +10,17 @@
 
 #include "oblivious_transfer.h"
 
+#define RAND_MESSAGE_SIZE_BITS 128
+#define CHECK_RW(result, expected, message) \
+    if(result != expected){ \
+        perror(message); \
+        exit(EXIT_FAILURE); \
+    }
+
 using namespace std;
 using namespace CryptoPP;
 
 void ot_send(int sockfd,  bytevector msg1, bytevector msg2 ){
-    (void)sockfd; (void)msg1;; (void)msg2;
     //Setup RSA
     AutoSeededRandomPool prng;
     RSA::PrivateKey private_key;
@@ -27,11 +35,92 @@ void ot_send(int sockfd,  bytevector msg1, bytevector msg2 ){
     const Integer  d = private_key.GetPrivateExponent();
     const Integer  e = private_key.GetPublicExponent();
 
-    char* modulus_buffer = new char[n.ByteCount()];
-    for(unsigned int i = 0; i < n.ByteCount(); i++){
-        modulus_buffer[i] = n.GetByte(i);
-    }
-    delete[] modulus_buffer;
+    //Send modulus
+    unsigned int buf_size = n.ByteCount();
+    size_t rc = write_aon(sockfd, (char*)&buf_size, sizeof(buf_size));
+    CHECK_RW(rc, sizeof(buf_size), "Failed to write all bytes of modulus size")
+    byte* buffer = new byte[buf_size];
+    n.Encode(buffer, buf_size);
+    rc = write_aon(sockfd, (char*)buffer, buf_size);
+    CHECK_RW(rc, buf_size, "Failed to write all bytes of modulus")
+    delete[] buffer;
+
+    //Send public exponent
+    buf_size = e.ByteCount();
+    rc = write_aon(sockfd, (char*)&buf_size, sizeof(buf_size));
+    CHECK_RW(rc, sizeof(buf_size), "Failed to write all bytes of exponent size")
+    buffer = new byte[buf_size];
+    e.Encode(buffer, buf_size);
+    rc = write_aon(sockfd, (char*)buffer, buf_size);
+    CHECK_RW(rc, buf_size, "Failed to write all bytes of exponent")
+    delete[] buffer;
+
+    //Generate two random messages
+    Integer rand_msg_1 = Integer();
+    Integer rand_msg_2 = Integer();
+    rand_msg_1.Randomize(prng, RAND_MESSAGE_SIZE_BITS);
+    rand_msg_2.Randomize(prng, RAND_MESSAGE_SIZE_BITS);
+    byte* rand_1 = new byte[rand_msg_1.ByteCount()];
+    byte* rand_2 = new byte[rand_msg_2.ByteCount()];
+    rand_msg_1.Encode(rand_1, rand_msg_1.ByteCount());
+    rand_msg_2.Encode(rand_2, rand_msg_2.ByteCount());
+
+    //Send random messages
+    buf_size = rand_msg_1.ByteCount();
+    rc = write_aon(sockfd, (char*)&buf_size, sizeof(buf_size));
+    CHECK_RW(rc, sizeof(buf_size), "Failed to write all bytes of random message 1 size")
+    rc = write_aon(sockfd, (char*)rand_1, buf_size);
+    CHECK_RW(rc, buf_size, "Failed to write all bytes of random message 1");
+    buf_size = rand_msg_2.ByteCount();
+    rc = write_aon(sockfd, (char*)&buf_size, sizeof(buf_size));
+    CHECK_RW(rc, sizeof(buf_size), "Failed to write all bytes of random message 2 size")
+    rc = write_aon(sockfd, (char*)rand_2, buf_size);
+    CHECK_RW(rc, buf_size, "Failed to write all bytes of random message 2")
+    delete[] rand_1;
+    delete[] rand_2;
+
+    //Receive receiver's blinded encryption
+    rc = read_aon(sockfd, (char*)&buf_size, sizeof(buf_size));
+    CHECK_RW(rc, sizeof(buf_size), "Failed to read all bytes from blinded encryption size")
+    buffer = new byte[buf_size];
+    rc = read_aon(sockfd, (char*)buffer, buf_size);
+    CHECK_RW(rc, buf_size, "Failed to read all bytes from blinded encryption")
+    Integer v = Integer(buffer, buf_size);
+    delete[] buffer;
+
+    //Calculate k's
+    Integer k0 = a_exp_b_mod_c((v - rand_msg_1), d, n);
+    Integer k1 = a_exp_b_mod_c((v - rand_msg_2), d, n);
+
+    //TODO: If weird errors show up during OT, check here for problems like endianness
+    //Convert messages to Integers and do math
+    Integer m0 = Integer((byte*)&msg1[0], msg1.size());
+    Integer m1 = Integer((byte*)&msg2[0], msg2.size());
+
+    Integer m0_prime = Integer();
+    Integer m1_prime = Integer();
+
+    m0_prime = m0 + k0;
+    m1_prime = m1 + k1;
+
+    //Send messages
+    buf_size = m0_prime.ByteCount();
+    buffer = new byte[buf_size];
+    m0_prime.Encode(buffer, buf_size);
+    rc = write_aon(sockfd, (char*)&buf_size, sizeof(buf_size));
+    CHECK_RW(rc, sizeof(buf_size), "Failed to write all bytes of m0' size")
+    rc = write_aon(sockfd, (char*)buffer, buf_size);
+    CHECK_RW(rc, buf_size, "Failed to write all bytes of m0'")
+    delete[] buffer;
+
+    buf_size = m1_prime.ByteCount();
+    buffer = new byte[buf_size];
+    m1_prime.Encode(buffer, buf_size);
+    rc = write_aon(sockfd, (char*)&buf_size, sizeof(buf_size));
+    CHECK_RW(rc, sizeof(buf_size), "Failed to write all bytes of m1' size")
+    rc = write_aon(sockfd, (char*)buffer, buf_size);
+    CHECK_RW(rc, buf_size, "Failed to write all bytes of m1'")
+    delete[] buffer;
 }
 
 bytevector ot_recv(int sockfd, uint8_t bit){
