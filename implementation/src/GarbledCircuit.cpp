07526772c9bd999ad0657ffc7aef224843810a32
Avi Weinstock <aweinstock314@gmail.com>
2016-04-24 14:22:08 -0400

    Add circuit garbling via Crypto++ (AES/Salsa20 parameterisable via #defines). Ensured that GarbledCircuit.cpp compiles.

--- implementation/src/GarbledCircuit.cpp
+++ implementation/src/GarbledCircuit.cpp
@@ -1,21 +1,42 @@
+#include <crypto++/filters.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+#include "Circuit.h"
 #include "GarbledCircuit.h"
+
+void encrypt(uint8_t *dst, const uint8_t *src, const uint8_t *k1, const uint8_t *k2) {
+    GARBLER_CIPHER::Encryption e1(k1, SEC_PARAM), e2(k2, SEC_PARAM);
+    CryptoPP::ArraySink sink(dst, SEC_PARAM+1);
+    CryptoPP::StreamTransformationFilter stf1(e1, &sink), stf2(e2, &sink);
+    stf2.PutMessageEnd(src, SEC_PARAM+1);
+    stf1.PutMessageEnd(src, SEC_PARAM+1);
+}
+
+void decrypt(uint8_t *dst, const uint8_t *src, const uint8_t *k1, const uint8_t *k2) {
+    GARBLER_CIPHER::Decryption e1(k1, SEC_PARAM), e2(k2, SEC_PARAM);
+    CryptoPP::ArraySink sink(dst, SEC_PARAM+1);
+    CryptoPP::StreamTransformationFilter stf1(e1, &sink), stf2(e2, &sink);
+    stf1.PutMessageEnd(src, SEC_PARAM+1);
+    stf2.PutMessageEnd(src, SEC_PARAM+1);
+}
 
 // This uses the "Single-Party Garbling Scheme" described in section 3.1 of the paper
 //  "Efficient Three-Party Computation from Cut-and-Choose" by Choi, Katz, Malozemoff, and Zikas
 // This is more efficient than the garbling scheme from "A Proof of Security of Yao's Protocol for Two-Party Computation" by Lindell and Pinkas
 //  since the receiver only needs to make 1 decryption per gate (down from 4), as well as space/communication savings from lack of padding/output maps
-GarbledCircuit::GarbledCircuit(Circuit c_) : c(c_) , zeros(c.wires.size()), ones(c.wires.size()), gates(c.wires.size()), lambdas(c.wires.size()) {
+GarbledCircuit::GarbledCircuit(Circuit c_) :
+    c(c_), zeros(c.wires.size()), ones(c.wires.size()), gates(c.wires.size()), lambdas(c.wires.size()) {
     int fd = open("/dev/urandom", O_RDONLY);
     uint8_t temp;
+    size_t i,j,k;
+    GateWire *w;
 
-    for (size_t i=0; i<c.wires.size(); i++) {
+    for (i=0; i<c.wires.size(); i++) {
         // produce random bytevectors for the keys
         zeros[i].resize(SEC_PARAM);
         ones[i].resize(SEC_PARAM);
-        read(fd, zeros[i].data(), SEC_PARAM);
-        read(fd, ones[i].data(), SEC_PARAM);
+        read_aon(fd, (char*)zeros[i].data(), SEC_PARAM);
+        read_aon(fd, (char*)ones[i].data(), SEC_PARAM);
 
 
         if(boost::get<OutputWire>(&c.wires[i])) {
@@ -23,12 +44,29 @@ GarbledCircuit::GarbledCircuit(Circuit c_) : c(c_) , zeros(c.wires.size()), ones
             lambdas[i] = 0;
         } else {
             // produce random bit for the blindings
-            read(fd, &temp, 1);
+            read_aon(fd, (char*)&temp, 1);
             // Extract a single bit of randomness from a full byte
             // test is correct to extract the MSB without bias, as demonstrated by the following haskell
             // ghci> length[0..127]==length[128..255]
             // True
-            lamdas[i] = temp<128 ? 1 : 0;
+            lambdas[i] = temp<128 ? 1 : 0;
+        }
+
+        // assumes a topological ordering, which is produced by generate_unsigned_compare_circuit
+        //  it would be more elegant to walk the circuit starting from the outputs, but this is easier (because boost)
+        if((w = boost::get<GateWire>(&c.wires[i]))) {
+            for(j = 0; j <= 1; j++) { for(k = 0; k <= 1; k++) {
+                // adapted from Figure 1 of Zikas et. al.
+                // P[g, j, k] <- Enc_{K_a^j, K_b^k}(K_g^\sigma concat \sigma)
+                // \sigma = G_g(\lambda_a xor j, \lambda_b xor k) xor \lambda_g
+                uint8_t *buf = &gates[i].values[j][k][0];
+                bool sigma = eval_truthtable(w->truth_table, lambdas[w->l] ^ j, lambdas[w->r] ^ k) ^ lambdas[i];
+#define KEY(wire, bit) ((bit) ? ones[(wire)] : zeros[(wire)]).data()
+                memcpy(buf, KEY(i, sigma), SEC_PARAM);
+                buf[SEC_PARAM] = sigma;
+                encrypt(buf, buf, KEY(w->l, j), KEY(w->r, k));
+#undef KEY
+            }}
         }
     }
 
@@ -47,12 +85,13 @@ template<class OT> void recv(int fd, bytevector y_) {
 
 template<class OT> void GarbledCircuit::send(int fd, bytevector x_) {
     bitvector x = unpack_bv(x_);
-    size i;
-    for(i=0, i<x.size(); i++) {
-        write(fd, x[i] ? ones[i] : zeros[i], SEC_PARAM);
+    size_t i;
+    for(i=0; i<x.size(); i++) {
+        write_aon(fd, (x[i] ? ones[i] : zeros[i]).data(), SEC_PARAM);
     }
-    for(i=0; i<reciever_bits; i++) {
-        OT::Send(fd, zeros[i], ones[i]); // todo: parallelism 
+    // x.size() == y.size() invariant should be enforced elsewhere
+    for(i=0; i<x.size(); i++) {
+        OT::send(fd, zeros[i], ones[i]); // todo: architechture for parallelism
     }
 }
 
--- implementation/src/GarbledCircuit.h
+++ implementation/src/GarbledCircuit.h
@@ -1,10 +1,18 @@
 #ifndef GARBLED_CIRCUIT_H
 #define GARBLED_CIRCUIT_H
 
+#include <crypto++/aes.h>
+#include <crypto++/modes.h>
+#include <crypto++/salsa.h>
 #include "utils.h"
 
 // statistical security parameter
-#define SEC_PARAM (128)
+#define SEC_PARAM (128/8)
+
+// TODO: unhackify with templates, test performance of both AES and Salsa20
+#define GARBLER_CIPHER CryptoPP::CTR_Mode<CryptoPP::AES>
+//#define GARBLER_CIPHER CryptoPP::Salsa20
+
 
 struct GarbledGate {
     uint8_t values[2][2][SEC_PARAM+1];
@@ -19,7 +27,7 @@ struct GarbledCircuit {
     bitvector lambdas; 
 
     // sender's constructor
-    GarbledCircuit::GarbledCircuit(Circuit c_);
+    GarbledCircuit(Circuit c_);
 
     // TODO: create recv's constructor
     template<class OT> void send(int fd, bytevector x_);
