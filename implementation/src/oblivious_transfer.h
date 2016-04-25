d7f7c8d54b97c545251a5fcf40028b301ab88e6e
Brian Sheedy <weableandbob@gmail.com>
2016-04-24 20:44:04 -0400

    classified RSA OT

--- implementation/src/oblivious_transfer.h
+++ implementation/src/oblivious_transfer.h
@@ -5,7 +5,12 @@
 
 #include "utils.h"
 
-void ot_send(int sockfd,  bytevector msg1, bytevector msg2 );
-bytevector ot_recv(int sockfd, uint8_t bit);
+class RSAObliviousTransfer{
+public:
+    static void send(int sockfd,  bytevector msg1, bytevector msg2 );
+    static bytevector recv(int sockfd, uint8_t bit);
+};
+/*void ot_send(int sockfd,  bytevector msg1, bytevector msg2 );
+bytevector ot_recv(int sockfd, uint8_t bit);*/
 
 #endif
